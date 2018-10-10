7cbe6fa7208a39dfdf581f532c2b6d3c1f7b8e42
kakuuuz <kakuuuz23@gmail.com>
2018-10-10 22:44:45 +0530

    created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomepageComponent },
+];
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    FooterComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
   providers: [],
