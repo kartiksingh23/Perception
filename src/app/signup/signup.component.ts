49222c1d83949c73e88d617af0d3c171e6e606f1
kakuuuz <kakuuuz23@gmail.com>
2018-10-12 03:15:45 +0530

    made the app responsive

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -21,7 +21,7 @@ export class SignupComponent implements OnInit {
           dob: ['', [Validators.required]],
           email: ['', [Validators.required, Validators.email]],
           password: ['', [Validators.required, Validators.minLength(6)]],
-          contactNumber: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern("\d*")]]
+          contactNumber: ['', [Validators.required, Validators.maxLength(10), Validators.minLength(10), Validators.pattern('[0-9]{0,10}')]]
       });
   }
 
@@ -29,7 +29,6 @@ export class SignupComponent implements OnInit {
   get f() { return this.registerForm.controls; }
 
   onSubmit() {
-    console.log(this.f.contactNumber);
       this.submitted = true;
       // stop here if form is invalid and console unsuccessful message
       if (this.registerForm.invalid) {
